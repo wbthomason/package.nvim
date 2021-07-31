4138b5f6b41763735ac108035c0df03b93493ed8
Zhi Fan <chenzhifan9@gmail.com>
2021-07-31 07:48:49 +0200

    Up to date

--- tests/packer_snapshot_spec.lua
+++ tests/packer_snapshot_spec.lua
@@ -1,5 +1,8 @@
 local a = require('plenary.async_lib.tests')
+local with = require('plenary.context_manager').with
+local open = require('plenary.context_manager').open
 local path = require('plenary.path')
+local strings = require('plenary.strings')
 local describe = require('plenary.busted').describe
 local it = require('plenary.busted').it
 local await = require('packer.async').wait
@@ -10,15 +13,20 @@ local use = packer.use
 local fmt = string.format
 
 local config = {
-    snapshot_path = vim.fn.stdpath("cache") .. "/" .. "packer"
+    snapshot_path = vim.fn.stdpath("cache") .. "/" .. "packer",
+    display = {
+        non_interactive = true,
+        open_cmd = '65vnew \\[packer\\]',
+        }
 }
 
 local spec = {'wbthomason/packer.nvim'}
 
-a.describe('Packer testing packer.snapshot()', function ()
+a.describe('Packer testing ', function ()
     local snapshot_name = "test"
     local test_path = config.snapshot_path .. "/" .. snapshot_name
     local snapshot = require 'packer.snapshot'
+    local p = path:new(test_path)
 
     before_each(function ()
         local _packer = packer.startup(function ()
@@ -27,9 +35,34 @@ a.describe('Packer testing packer.snapshot()', function ()
         _packer.__manage_all()
     end)
 
-    a.it(fmt("create snapshot '%s'", test_path), function ()
-        await(snapshot(test_path, {spec}))
-        local p = path:new(test_path)
-        assert.True(p:exists())
+    a.describe('packer.snapshot()', function ()
+        a.it(fmt("create snapshot with installed plugins'%s'", test_path), function ()
+            await(snapshot(test_path, {spec}))
+            assert.True(p:exists())
+        end)
+
+        after_each(function ()
+            p:rm()
+        end)
+    end)
+
+    a.describe('packer.rollback()', function ()
+        a.it(fmt("restore plugin to previous state"), function ()
+            local rev = 'c8c0600'
+            with(open(test_path, 'w+'), function (file)
+                file:write(fmt("%s %s", "packer.nvim", rev))
+            end)
+
+            packer.rollback(snapshot_name)
+            p:rm()
+            assert.False(p:exists())
+            await(snapshot(test_path, {spec}))
+--
+--            local res = with(open(test_path), function (file)
+--                return strings.strcharpart(file:read(), 11)
+--            end)
+
+            assert.equal(rev, res)
+        end)
     end)
 end)
